4f8e0fa52a28c074e3835dcc07186798544c612c
abdelrahman <abdelrahmanahmad78780@gmail.com>
2024-03-12 17:43:10 +0200

    my project is 0x01-python-if_else_loops_functions done today .

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
-###########################
+for a in "abcdefghijklmnopqrstuvwxyz":
+    if a != "q" and a != "e":
+        print("{}".format(a), end='')
